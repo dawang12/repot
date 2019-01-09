21fb9c33dad7da1fd33632bf743c73a8d7683575
dawang <dawang@qq.com>
2019-01-09 18:33:56 +0800

    gaga

--- src/com/itheima/hello/World.java
+++ src/com/itheima/hello/World.java
@@ -4,10 +4,6 @@ public class World {
 
 	public static void main(String[] args) {
  		System.out.println("helloworld");
- 		System.out.println("helloworld");
- 		System.out.println("helloworld");
- 		System.out.println("helloworld");
- 		System.out.println("helloworld");
- 		System.out.println("helloworld");
+
 	}
 }
