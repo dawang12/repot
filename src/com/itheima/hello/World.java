9b90b3e729ed2665597926e171fc164f83c837a1
dawang <dawang@qq.com>
2019-01-09 16:52:47 +0800

    初始化maven

---
+++ src/com/itheima/hello/World.java
@@ -0,0 +1,13 @@
+package com.itheima.hello;
+
+public class World {
+
+	public static void main(String[] args) {
+ 		System.out.println("helloworld");
+ 		System.out.println("helloworld");
+ 		System.out.println("helloworld");
+ 		System.out.println("helloworld");
+ 		System.out.println("helloworld");
+ 		System.out.println("helloworld");
+	}
+}
